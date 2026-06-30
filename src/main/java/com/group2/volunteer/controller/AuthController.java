dfcfbac10a9d73a2f47e0225c505e89bc7eca000
Hiyu <vuduchieu629@gmail.com>
2026-06-30 09:28:42 +0700

    Them phan register volunteer va sua lai mot vai cho html

--- src/main/java/com/group2/volunteer/controller/AuthController.java
+++ src/main/java/com/group2/volunteer/controller/AuthController.java
@@ -1,15 +1,18 @@
 package com.group2.volunteer.controller;
 
 import com.group2.volunteer.dto.LoginDTO;
+import com.group2.volunteer.dto.RegisterDTO;
 import com.group2.volunteer.entity.Project;
 import com.group2.volunteer.entity.User;
 import com.group2.volunteer.service.ProjectService;
 import com.group2.volunteer.service.SavedProjectService;
 import com.group2.volunteer.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +50,30 @@ public class AuthController {
         return "common/homepage";
     }
 
+    @GetMapping("/register/volunteer")
+    public String regVolunteerForm(Model model) {
+        model.addAttribute("registerDTO", new RegisterDTO());
+        return "common/register_volunteer";
+    }
+
+    @PostMapping("/register/volunteer")
+    public String saveVolunteer(@Valid @ModelAttribute("registerDTO") RegisterDTO registerDTO,
+                                BindingResult result,
+                                Model model) {
+        if (result.hasErrors()) {
+            return "common/register_volunteer";
+        }
+
+        try {
+            userService.registerVolunteer(registerDTO);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "common/register_volunteer";
+        }
+
+        return "redirect:/login?success=RegisterSuccess";
+    }
+
     @GetMapping("/login")
     public String showLoginPage() {
         return "common/login";
@@ -58,6 +85,11 @@ public class AuthController {
 
         if (user == null) {
             model.addAttribute("error", "Username or password is incorrect!");
+            return "common/login";
+        }
+
+        if ("PENDING".equals(user.getStatus())) {
+            model.addAttribute("error", "Your account is not verified!");
             return "common/login";
         }
 
