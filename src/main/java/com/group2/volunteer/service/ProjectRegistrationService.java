bff8cd27cc550d8efa0192b0378dc3cd75c3de4a
Nguyen Sang <sangddk2k6@gmail.com>
2026-07-20 23:58:55 +0700

    thêm đóng ứng tuyển, danh sách ứng viên

--- src/main/java/com/group2/volunteer/service/ProjectRegistrationService.java
+++ src/main/java/com/group2/volunteer/service/ProjectRegistrationService.java
@@ -1,5 +1,13 @@
 package com.group2.volunteer.service;
 
+import com.group2.volunteer.entity.ProjectRegistration;
+
+import java.util.List;
+
 public interface ProjectRegistrationService {
+    List<ProjectRegistration> getProjectRegistrations(Long projectId, Long organizerId);
+    Long countProjectRegistrations(Long projectId, Long organizerId);
+    void approveRegistration(Long projectId, Long registrationId, Long organizerId);
+    void rejectRegistration(Long projectId, Long registrationId, Long organizerId);
 
 }
