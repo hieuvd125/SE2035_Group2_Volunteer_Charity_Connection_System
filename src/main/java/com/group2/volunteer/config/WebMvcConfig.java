e0d93da02d8de0c293de2cf29d1e7c7d83bfc0bc
bento <Minhthuanvu732@gmail.com>
2026-07-21 03:52:35 +0700

    added event update

--- src/main/java/com/group2/volunteer/config/WebMvcConfig.java
+++ src/main/java/com/group2/volunteer/config/WebMvcConfig.java
@@ -3,12 +3,19 @@ package com.group2.volunteer.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private RoleInterceptor roleInterceptor;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
